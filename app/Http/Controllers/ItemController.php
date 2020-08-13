835a7eaaa4359b1d0f8fec24e870832b70b9a34f
NayZawAg <nayzawaung.nicky@gmail.com>
2020-08-13 11:51:35 +0630

    show table, detail (modal), edit and delete for item

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -17,7 +17,9 @@ class ItemController extends Controller
      */
     public function index()
     {
-        return view ('backend.items.index');
+        $items=Item::all();
+        // dd($items);
+        return view ('backend.items.index',compact('items'));
     }
 
     /**
@@ -86,7 +88,9 @@ class ItemController extends Controller
      */
     public function show($id)
     {
-        return view ('backend.items.show');
+        $item=Item::find($id);
+        // dd($item);
+        return view ('backend.items.show',compact('item'));
     }
 
     /**
@@ -97,7 +101,11 @@ class ItemController extends Controller
      */
     public function edit($id)
     {
-        return view ('backend.items.edit');
+        $brands=Brand::all();
+        $subcategories=Subcategory::all();
+        $item=Item::find($id); 
+        // dd($item);
+        return view ('backend.items.edit',compact('brands','subcategories','item'));
     }
 
     /**
@@ -109,7 +117,46 @@ class ItemController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // dd($request);
+        //validation
+
+        $request->validate([
+            'item_codeno'=>'required|min:4',
+            'item_name'=>'required',
+            'item_photo'=>'sometimes',
+            'unit_price'=>'required',
+            'discount_price'=>'required',
+            'brand_id'=>'required',
+            'subcategory_id'=>'required',
+            'item_des'=>'required',
+        ]);
+        //if include file, upload
+        if ($request->hasFile('item_photo')) {
+          
+         $imageName=time().'.'.$request->item_photo->extension();
+            
+        $request->item_photo->move(public_path('backend/itemimg'),$imageName);
+        $myfile='backend/itemimg/'.$imageName;
+        }
+
+        //delete old photo (unlink)
+        else{
+            $myfile=$request->oldphoto;
+        }
+        //data update
+        $item= Item::find($id); //setup class
+        $item->codeno=$request->item_codeno;
+        $item->name=$request->item_name;
+        $item->photo=$myfile;
+        $item->price=$request->unit_price;
+        $item->discount=$request->discount_price;
+        $item->description=$request->item_des;
+        $item->brand_id=$request->brand_id;
+        $item->subcategory_id=$request->subcategory_id;
+        $item->save();
+
+        // Redirect
+        return redirect()->route('items.index');
     }
 
     /**
@@ -120,6 +167,10 @@ class ItemController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $item=Item::find($id);
+        $item->delete();
+        //redirect
+        return redirect()->route('items.index');
+
     }
 }
--- resources/views/backend/items/show.blade.php
+++ resources/views/backend/items/show.blade.php
@@ -1,9 +1,22 @@
 @extends('backendtemplate')
-
+@section('title','Item Detail')
 @section('content')
 
 	<div class="container-fluid">
-		<h2>Item Detail (Your UI)</h2>
+		<h2>Item Detail</h2>
+	<div class="row">
+		<div class="col-md-4">
+			<img src="{{asset($item->photo)}}" class="img-fluid w-75 h-75">
+		</div>
+		<div class="col-md-8">
+			<h5>Product Name: <span>{{$item->name}}</span></h5>
+			<h5>Product Code: <span>{{$item->codeno}}</span></h5>
+			<h5>Product Price: <span>{{$item->price}}</span></h5>
+			<h5>Product Discount: <span>{{$item->discount}}</span></h5>
+			<h5>Description: <br><span>{{$item->description}}</span></h5>
+		</div>
+	</div>
+
 
 	</div>
 @endsection
